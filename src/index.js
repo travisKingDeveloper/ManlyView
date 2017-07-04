9d62eda060315950c6f7023aee4226c516d3cad4
Travis King <travisking@customers-mbp.attlocal.net>
2017-07-03 23:42:43 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import registerServiceWorker from './util/registerServiceWorker'
+
+ReactDOM.render( <App/> , document.getElementById('root'))
+registerServiceWorker()
