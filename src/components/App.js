b70ff4819d893d5d9853dc36a3daad7408a2b192
Travis King <travisking@customers-mbp.attlocal.net>
2017-07-07 22:11:04 -0400

    Set up project structure. Made App render MaintenanceView and ManlyView, after a more thought I could change this up

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
-import logo from '../assets/logo.svg'
-import './App.css'
-import ManCave from './ManCave'
+import MaintenanceView from './MaintenanceView'
+import ManlyView from './ManlyView'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to The Manly View</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/components/App.js</code> and save to reload.
-        </p>
-        <a href="#" className="button">I'm a Button</a>
-        <ManCave />
-      </div>
-    )
-  }
+
+    isUnderMaintenance(){
+        return true
+    }
+
+    render() {
+        if(this.isUnderMaintenance()) {
+            return (
+              <MaintenanceView />
+            )
+        } else {
+            return ( 
+              <ManlyView />
+            )
+        }
+    }
 }
 
 export default App
---
+++ src/components/MaintenanceView/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MaintenanceView extends Component {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default MaintenanceView
